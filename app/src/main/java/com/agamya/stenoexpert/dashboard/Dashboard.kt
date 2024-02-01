1990cff0490a21b770c559a00fd5cb2db3ece3cb
suraj kumar <surajkumar969348@gmail.com>
2024-02-02 00:39:36 +0530

    dashboard
    dashboard

---
+++ app/src/main/java/com/agamya/stenoexpert/dashboard/Dashboard.kt
@@ -0,0 +1,12 @@
+package com.agamya.stenoexpert.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.agamya.stenoexpert.R
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
