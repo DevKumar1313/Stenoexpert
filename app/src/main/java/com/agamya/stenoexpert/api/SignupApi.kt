f04a29e66fc4a203daf154366f8a969f3a53b58e
Kallgit22 <shivmouryacse@gmail.com>
2024-02-02 22:29:15 +0530

    Optimization

--- app/src/main/java/com/agamya/stenoexpert/api/SignupApi.kt
+++ app/src/main/java/com/agamya/stenoexpert/api/SignupApi.kt
@@ -1,4 +1,5 @@
 package com.agamya.stenoexpert.api
 
 class SignupApi {
+
 }
--- app/src/main/java/com/agamya/stenoexpert/dashboard/Dashboard.kt
+++ app/src/main/java/com/agamya/stenoexpert/dashboard/Dashboard.kt
@@ -2,11 +2,46 @@ package com.agamya.stenoexpert.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.agamya.stenoexpert.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class Dashboard : AppCompatActivity() {
+    private lateinit var bottomNav:BottomNavigationView
+    private lateinit var slideNav:NavigationView
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        bottomNav = findViewById(R.id.bottom)
+        slideNav = findViewById(R.id.slideNav)
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        val toggle = 
+        loadFragment(Home())
+
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.home -> loadFragment(Home())
+                R.id.course -> loadFragment(Course())
+                R.id.download -> loadFragment(Download())
+                R.id.helpDesk -> loadFragment(HelpDesk())
+            }
+            return@setOnItemSelectedListener true
+        }
+
+
+    }
+
+    //Function for Load Fragment on Frame Layout
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.Frame,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
