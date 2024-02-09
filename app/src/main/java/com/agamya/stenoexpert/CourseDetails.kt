0af5b353efb4bc943137763d8c6e9e4f2fa2c12d
Kallgit22 <shivmouryacse@gmail.com>
2024-02-09 23:37:17 +0530

    Optimised

--- app/src/main/java/com/agamya/stenoexpert/CourseDetails.kt
+++ app/src/main/java/com/agamya/stenoexpert/CourseDetails.kt
@@ -3,7 +3,7 @@ package com.agamya.stenoexpert
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Coursedetails : AppCompatActivity() {
+class CourseDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coursedetails)
