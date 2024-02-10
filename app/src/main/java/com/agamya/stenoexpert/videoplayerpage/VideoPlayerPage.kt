5b0c9cf88519c63b61a17090dbb9d0a50b6512e9
Kallgit22 <shivmouryacse@gmail.com>
2024-02-10 13:38:06 +0530

    Video Playing Feature(Basic)
    You can See a Video Which is place on Server

--- app/src/main/java/com/agamya/stenoexpert/videoplayerpage/VideoPlayerPage.kt
+++ app/src/main/java/com/agamya/stenoexpert/videoplayerpage/VideoPlayerPage.kt
@@ -1,7 +1,9 @@
 package com.agamya.stenoexpert.videoplayerpage
 
-import android.net.Uri
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
+import android.view.View
 import android.widget.MediaController
 import android.widget.ProgressBar
 import android.widget.Toast
@@ -10,7 +12,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
 import com.agamya.stenoexpert.R
-import java.io.IOException
 
 
 class VideoPlayerPage : AppCompatActivity() {
@@ -19,7 +20,7 @@ class VideoPlayerPage : AppCompatActivity() {
     private lateinit var mediaController: MediaController
     private lateinit var progressBar: ProgressBar
     private lateinit var toolbar:Toolbar
-    private val url = "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1"
+    private val url = "http://154.41.254.57:3000"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_player)
@@ -35,18 +36,46 @@ class VideoPlayerPage : AppCompatActivity() {
 
         Toast.makeText(this, "Design By Neha", Toast.LENGTH_SHORT).show()
 
-        try {
-            mediaController = MediaController(this)
-            mediaController.setAnchorView(videoView)
-            videoView.setMediaController(mediaController)
-            val uri = Uri.parse(url)
-            videoView.setVideoURI(uri)
-            videoView.requestFocus()
-            videoView.start()
-
-        }catch (e: IOException){
-            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
-        }
+        // Execute the AsyncTask
+        NetworkTask().execute()
 
     }
+
+
+    @SuppressLint("StaticFieldLeak")
+    private inner class NetworkTask : AsyncTask<Void?, Void?, Void?>() {
+
+        @Deprecated("Deprecated in Java")
+        override fun doInBackground(vararg params: Void?): Void? {
+            // Your network-related code here
+            try {
+                // Simulate a delay
+                Thread.sleep(2000)
+                // Continue with the rest of your network-related code
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onPostExecute(result: Void?) {
+            // After the network operation is complete, start playing the video
+            playVideo()
+            progressBar.visibility = View.GONE
+        }
+    }
+    private fun playVideo() {
+        // Replace "your_video_url" with the actual URL of your video val videoUrl = "your_video_url"
+        videoView.setVideoPath(url)
+        // Create media controller
+        mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        // Start playing the video
+        videoView.start()
+    }
+
+
 }
+
